f8a1524be0b58aa935aeac01440e8f6d75e137ab
sael-you <saad.el.yousfi.95@gmail.com>
2021-05-15 00:16:54 +0000

    First step

---
+++ Lesson03/script.js
@@ -0,0 +1,27 @@
+const scene = new THREE.Scene();
+
+//Red Cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 'cyan' });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+//Size
+const sizes = {
+    width: 800,
+    height: 600
+};
+
+//Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+//Renderer
+const canvas = document.querySelector('.webgl');
+const renderer = new THREE.WebGLRenderer({
+    canvas
+});
+
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
