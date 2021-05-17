0fb9f8ae5e5e15c495b00000cc2aa1bb439cbe28
sael-you <saad.el.yousfi.95@gmail.com>
2021-05-17 12:29:27 +0100

    eighth step

--- Lesson10/src/script.js
+++ Lesson10/src/script.js
@@ -1,13 +1,24 @@
 import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import gsap from 'gsap';
+import * as dat from 'dat.gui';
+import { Mesh } from 'three';
+
+//Debug UI
+const gui = new dat.GUI({ closed: true });
+const parameters = {
+    color: "#ff0000",
+    spin: () => {
+        gsap.to(group.rotation, { y: group.rotation.y + Math.PI * 2 });
+    }
+};
 
 //Cursor
 const cursos = {
     x: 0,
     y: 0
 };
-
 window.addEventListener('mousemove', (event) => {
     cursos.x = event.clientX / sizes.width - 0.5;
     cursos.y = event.clientY / sizes.height - 0.5;
@@ -19,39 +30,61 @@ const scene = new THREE.Scene();
 
 //Objects
 const group = new THREE.Group();
-
 const cube1 = new THREE.Mesh(
     new THREE.BoxGeometry(1, 1, 1),
     new THREE.MeshBasicMaterial({
-        color: 'green',
+        color: '#008000',
         wireframe: true
     })
 );
 cube1.position.set(0, 1, 0);
+cube1.name = 'Top Cube';
 group.add(cube1);
-
 const cube2 = new THREE.Mesh(
     new THREE.BoxGeometry(1, 1, 1),
     new THREE.MeshBasicMaterial({
-        color: 'red',
+        color: '#ff0000',
         wireframe: true
     })
 );
 cube2.position.set(1, 0, 0);
+cube2.name = 'Right Cube';
 group.add(cube2);
-
 const cube3 = new THREE.Mesh(
     new THREE.BoxGeometry(1, 1, 1),
     new THREE.MeshBasicMaterial({
-        color: 'blue',
+        color: '#0000ff',
         wireframe: true
     })
 );
 cube3.position.set(0, 0, 1);
+cube3.name = 'Left Cube';
 group.add(cube3);
-
-// group.rotation.set(0, 0.7, 0);
 scene.add(group);
+
+//Debug group
+gui
+    .add(group.position, 'y')
+    .min(-3)
+    .max(3)
+    .step(0.001)
+    .name('group Y');
+
+group.children.forEach(element => {
+    gui
+        .addFolder(element.name)
+        .add(element.material, 'wireframe')
+
+
+    gui
+        .addColor(parameters, 'color')
+        .onChange(() => {
+            element.material.color.set(parameters.color);
+        });
+});
+
+gui
+    .add(parameters, 'spin')
 
 //Axes
 const axesHelper = new THREE.AxesHelper(4);
@@ -63,7 +96,7 @@ const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 };
-
+const canvas = document.querySelector('.webgl');
 window.addEventListener('resize', () => {
     //Update Sizes
     sizes.width = window.innerWidth;
@@ -74,10 +107,7 @@ window.addEventListener('resize', () => {
     //Update Renderer
     renderer.setSize(sizes.width, sizes.height);
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
 });
-const canvas = document.querySelector('.webgl');
-
 window.addEventListener('dblclick', () => {
     const fullScreen = document.fullscreenElement || document.webkitFullscreenElement;
     if (!fullScreen) {
@@ -95,20 +125,13 @@ window.addEventListener('dblclick', () => {
 
 //Camera
 const camera = new THREE.PerspectiveCamera(60, sizes.width / sizes.height, 0.001, 100);
-
-// const aspectRation = sizes.width / sizes.height;
-// const camera = new THREE.OrthographicCamera(-1 * aspectRation, 1 * aspectRation, 1, -1, 0.001, 100);
 camera.position.z = 3;
-// camera.position.y = 3;
-// camera.position.x = 3;
 camera.lookAt(group.position);
 scene.add(camera);
 
 //Controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
-// controls.target.y = 2;
-// controls.update();
 
 //Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -116,23 +139,25 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
 //DeltaTime
 const clock = new THREE.Clock();
 
 //Animation
 const Animation = () => {
     const deltaTime = clock.getElapsedTime();
-
     //update camera
 
     //Update Controls
     controls.update();
 
-    group.rotation.y = Math.cos(deltaTime * Math.PI * 1);
-    cube1.rotation.y = Math.sin(deltaTime * Math.PI * 4);
-    cube2.rotation.x = Math.sin(-deltaTime * Math.PI * 4);
-    cube3.rotation.x = Math.sin(deltaTime * Math.PI * 4);
+    //Updae Objects
+    // group.rotation.y = Math.cos(deltaTime * Math.PI * 1);
+    // cube1.rotation.y = Math.sin(deltaTime * Math.PI * 4);
+    // cube2.rotation.x = Math.sin(-deltaTime * Math.PI * 4);
+    // cube3.rotation.x = Math.sin(deltaTime * Math.PI * 4);
 
+    //Render
     renderer.render(scene, camera);
     window.requestAnimationFrame(Animation);
 };
