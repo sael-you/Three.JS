9bdb997f74734b5436e9c3d2dd2ca25f8b6338b4
sael-you <saad.el.yousfi.95@gmail.com>
2021-05-20 20:40:00 +0100

    Hunted House

---
+++ Lesson16/src/script.js
@@ -0,0 +1,158 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
+
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader()
+
+/**
+ * House
+ */
+// House container
+const house = new THREE.Group();
+scene.add(house);
+
+//walls
+const walls = new THREE.Mesh(
+    new THREE.BoxBufferGeometry(4, 2.5, 4),
+    new THREE.MeshStandardMaterial({
+        color: '#ac8e82'
+    })
+);
+walls.position.y = 2.5 / 2
+house.add(walls);
+
+// Roof
+
+// Door
+
+// Floor
+const floor = new THREE.Mesh(
+    new THREE.PlaneGeometry(20, 20),
+    new THREE.MeshStandardMaterial({
+        color: "#a9c388"
+    })
+)
+floor.rotation.x = -Math.PI * 0.5
+floor.position.y = 0
+scene.add(floor)
+
+// Bushes
+
+/**
+ * Lights
+ */
+// Ambient light
+const ambientLight = new THREE.AmbientLight(0xffffff, 1)
+gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001)
+scene.add(ambientLight)
+
+// Directional light
+
+// Door light
+
+
+/**
+ * Ghosts
+ */
+
+/**
+ * Fog
+ */
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 4
+camera.position.y = 2
+camera.position.z = 5
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime()
+
+    // Ghosts
+    // const ghost1Angel = elapsedTime * 0.5
+    // ghost1.position.x = Math.cos(ghost1Angel) * 4
+    // ghost1.position.y = Math.sin(elapsedTime * 3)
+    // ghost1.position.z = Math.sin(ghost1Angel) * 4
+
+    // const ghost2Angel = elapsedTime * 0.32
+    // ghost2.position.x = Math.cos(ghost2Angel) * 5
+    // ghost2.position.y = Math.sin(elapsedTime * 4) + Math.sin(elapsedTime * 2.5)
+    // ghost2.position.z = Math.sin(ghost2Angel) * 5
+
+    // const ghost3Angel = elapsedTime * 0.18
+    // ghost3.position.x = Math.cos(ghost3Angel) * (7 + Math.sin(elapsedTime * 0.32))
+    // ghost3.position.y = Math.sin(elapsedTime * 4) + Math.sin(elapsedTime * 2.5)
+    // ghost3.position.z = Math.sin(ghost3Angel) * (7 + Math.sin(elapsedTime * 0.5))
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
