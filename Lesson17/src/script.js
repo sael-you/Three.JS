784e828c4db7105007f193ff724acb287c1d3c8a
sael-you <saad.el.yousfi.95@gmail.com>
2021-05-21 12:39:47 +0100

    Particles

--- Lesson17/src/script.js
+++ Lesson17/src/script.js
@@ -5,33 +5,40 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 //Scene
 const scene = new THREE.Scene();
 
+//Textures
+const textureLoader = new THREE.TextureLoader();
+const particlesTexture = textureLoader.load('/particlePack_1.1/PNG (Transparent)/circle_02.png');
 
-//Objects
-const group = new THREE.Group();
+//Particles
 
-const cube1 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 'green' })
-);
-cube1.position.set(0, 1, 0);
-group.add(cube1);
+//Material
+const particlesMaterial = new THREE.PointsMaterial();
+// particlesMaterial.map = particlesTexture;
+particlesMaterial.transparent = true;
+particlesMaterial.alphaMap = particlesTexture;
+particlesMaterial.size = 0.1;
+particlesMaterial.sizeAttenuation = true;
+// particlesMaterial.alphaTest = 0.001;
+particlesMaterial.depthWrite = false;
+particlesMaterial.vertexColors = true;
 
-const cube2 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 'red' })
-);
-cube2.position.set(1, 0, 0);
-group.add(cube2);
 
-const cube3 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 'blue' })
-);
-cube3.position.set(0, 0, 1);
-group.add(cube3);
+//buffer
+const geometry = new THREE.BufferGeometry();
+const count = 50000;
+const verteces = new Float32Array(count * 3);
+const colors = new Float32Array(count * 3);
 
-// group.rotation.set(0, 0.7, 0);
-scene.add(group);
+for (let i = 0; i < count * 3; i++) {
+    verteces[i] = (Math.random() - 0.5) * 10;
+    colors[i] = Math.random();
+}
+
+geometry.setAttribute('position', new THREE.BufferAttribute(verteces, 3));
+geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
+const mesh = new THREE.Points(geometry, particlesMaterial);
+
+scene.add(mesh);
 
 //Canvas
 const sizes = {
@@ -60,7 +67,6 @@ const camera = new THREE.PerspectiveCamera(60, sizes.width / sizes.height, 0.001
 
 
 camera.position.z = 3;
-camera.lookAt(group.position);
 scene.add(camera);
 
 //Controls
@@ -81,6 +87,14 @@ const clock = new THREE.Clock();
 const Animation = () => {
     const deltaTime = clock.getElapsedTime();
 
+    //update particles
+    // mesh.rotation.y = deltaTime * 0.2;
+    for (let i = 0; i < count; i++) {
+        const i3 = i * 3;
+        const x = geometry.attributes.position.array[i3];
+        geometry.attributes.position.array[i3 + 1] = Math.sin(deltaTime + x);
+    }
+    geometry.attributes.position.needsUpdate = true;
     //Update Camera
 
     //Update Controls
